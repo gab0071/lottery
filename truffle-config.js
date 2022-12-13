3bad2ac741f35a9f7ca0d667f1d81f47efcf5ff0
gab0071 <jeicarm7@gmail.com>
2022-12-13 18:35:38 -0500

    github actions

--- truffle-config.js
+++ truffle-config.js
@@ -41,8 +41,8 @@ module.exports = {
         // tab if you use this network and you must also set the `host`, `port` and `network_id`
         // options below to some value.
         development: {
-            host: '172.30.208.1', // Localhost (default: none)
-            port: 7545, // Standard Ethereum port (default: none)
+            host: 'http://127.0.0.1', // Localhost (default: none)
+            port: 8545, // Standard Ethereum port (default: none)
             network_id: '*', // Any network (default: none)
         },
          // Binance Smart Chain Testnet (BSC)
